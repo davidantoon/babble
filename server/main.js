f86285b7019832b8ac12b9005c7f86c16c5ff6a4
David Antoon <david.antoon@hotmail.com>
2017-09-06 20:43:32 +0300

    init

---
+++ server/main.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const app = express();
+
+
+
+app.get('/', function (req, res) {
+    res.send('Hello World!')
+});
+
+app.post('/', function (req, res) {
+    res.send('Got a POST request')
+});
+
+app.listen(8080, function () {
+    console.log('Example app listening on port 3000!')
+});
